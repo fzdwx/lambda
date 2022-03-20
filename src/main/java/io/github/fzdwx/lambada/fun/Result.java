ef21f85899e94d07356e10a90f4689f982af6fae
fzdwx <likelovec@gmail.com>
2022-03-20 17:10:14 +0800

    ref bind

---
+++ src/main/java/io/github/fzdwx/lambada/fun/Result.java
@@ -0,0 +1,16 @@
+package io.github.fzdwx.lambada.fun;
+
+/**
+ * @author <a href="mailto:likelovec@gmail.com">fzdwx</a>
+ * @date 2022/3/20 16:17
+ */
+public interface Result<Element> {
+
+    void then(Hooks<Element> hooks);
+
+    static <Element> Result<Element> of(Element element) {
+        return (h) -> {
+
+        };
+    }
+}
