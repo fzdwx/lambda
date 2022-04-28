f228350e59fcb722e3d3982a113902672930001a
weiZhen <likelovec@gmail.com>
2022-04-28 10:43:10 +0800

    feat read str

--- src/main/java/io/github/fzdwx/lambada/Io.java
+++ src/main/java/io/github/fzdwx/lambada/Io.java
@@ -12,6 +12,8 @@ import java.io.ByteArrayInputStream;
  */
 public interface Io {
 
+    String EMPTY = "";
+
     /**
      * 包装 字节数组到 字节输入流
      *
@@ -21,5 +23,66 @@ public interface Io {
      */
     static ByteArrayInputStream wrap(byte @NonNull [] bytes) {
         return new ByteArrayInputStream(bytes);
+    }
+
+    /**
+     * @see #read(String, int, int)
+     */
+    static String read(String str, int length) {
+        return read(str, 0, length);
+    }
+
+    /**
+     * @see #read(String, int, int)
+     */
+    static String readSuf(String str, int startIndexInclude) {
+        return read(str, startIndexInclude, str.length());
+    }
+
+    /**
+     * 返回作为此字符串的子字符串的字符串。子字符串从指定的beginIndex开始并延伸到索引endIndex - 1处的字符。
+     * 因此子字符串的长度是endIndex-beginIndex
+     *
+     * @param str              str
+     * @param fromIndexInclude 起始索引（包含）
+     * @param toIndexExclude   结束索引（不包含）
+     * @return {@link String }
+     */
+    static String read(String str, int fromIndexInclude, int toIndexExclude) {
+        if (str == null) {
+            return "";
+        }
+
+        int len = str.length();
+
+        if (fromIndexInclude < 0) {
+            fromIndexInclude = len + fromIndexInclude;
+            if (fromIndexInclude < 0) {
+                fromIndexInclude = 0;
+            }
+        } else if (fromIndexInclude > len) {
+            fromIndexInclude = len;
+        }
+
+        if (toIndexExclude < 0) {
+            toIndexExclude = len + toIndexExclude;
+            if (toIndexExclude < 0) {
+                toIndexExclude = len;
+            }
+        } else if (toIndexExclude > len) {
+            toIndexExclude = len;
+        }
+
+        if (toIndexExclude < fromIndexInclude) {
+            int tmp = fromIndexInclude;
+            fromIndexInclude = toIndexExclude;
+            toIndexExclude = tmp;
+        }
+
+        if (fromIndexInclude == toIndexExclude) {
+            return EMPTY;
+        }
+
+        return str.substring(fromIndexInclude, toIndexExclude);
     }
 }
