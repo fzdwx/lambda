cd7f9ec1fac1e97dde7f1ec2325b7f94a2ac30cf
weiZhen <likelovec@gmail.com>
2022-05-12 19:00:26 +0800

    temp

---
+++ src/test/java/io/github/fzdwx/lambada/lang/LineTest.java
@@ -0,0 +1,23 @@
+package io.github.fzdwx.lambada.lang;
+
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+
+/**
+ * @author <a href="mailto:likelovec@gmail.com">韦朕</a>
+ * @date 2022/5/12 17:15
+ */
+public class LineTest {
+
+    public static void main(String[] args) {
+        test_1();
+    }
+
+    static void test_1() {
+        final LineReader reader = LineReaderBuilder.builder().build();
+        while (true) {
+            final String s = reader.readLine("fzdwx> ");
+            System.out.println(s);
+        }
+    }
+}
